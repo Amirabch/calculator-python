a03cc003bd4eb1e84e80b28e3ea81bd04412c557
Amirabch <a.bouchiba@cu-elbayadh.dz>
2026-05-05 21:27:28 +0100

    setup calculator logic

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+a = float(input("Entrer le premier nombre: "))
+b = float(input("Entrer le deuxième nombre: "))
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+operation = input("Choisir (+, -, *, /): ")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if operation == "+":
+    print(a + b)
+elif operation == "-":
+    print(a - b)
+elif operation == "*":
+    print(a * b)
+elif operation == "/":
+    print(a / b)
+else:
+    print("Opération non valide")
